ea28c3fe25ff43477d765f6d3351724944c041cc
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2023-02-05 19:09:33 +0100

    Update ComputerBaseline.ps1

--- ComputerBaseline.ps1
+++ ComputerBaseline.ps1
@@ -18,7 +18,7 @@ $ToolFolder = "C:\_Tools"
 if (-not (Test-Path $ToolFolder)) { New-Item -Path $ToolFolder -ItemType Directory }
 
 #Create URL shortcut to https://github.com/dawah90
-$ShortcutPath = "C:\_Tools\Github-Dawah90.url"
+$ShortcutPath = "$ToolFolder\Github-Dawah90.url"
 if (-not (Test-Path $ShortcutPath))
 {
 $SourceFilePath = "https://github.com/dawah90"
@@ -28,3 +28,47 @@ $shortcut.TargetPath = $SourceFilePath
 Write-Output $ShortcutPath
 $shortcut.Save()
 }
+
+#Create Settings folder
+$SettingsFolderPath = "$ToolFolder\SettingsFolder.{ED7BA470-8E54-465E-825C-99712043E01C}"
+if (-not (Test-Path $SettingsFolderPath))
+{
+New-Item -Path $SettingsFolderPath -ItemType Directory
+}
+
+<# Windows Terminal Quake mode
+
+Windows Terminal
+Settings
+* Startup > Launch on machine startup > ON
+* Startup and choose "Default terminal application" > Windows-Terminal
+* Appearence > Hide terminal in notification area when it is minimized > ON
+* Actions > Add New > Show/Hide Quake Window > bind CTRL+SPACE
+* Profile powershell and cmd > enable "Run this profile as administrator"
+
+#>
+
+<# Move "invisible window"
+
+Select application window in task bar.
+Press "ALT+SPACE" and "F" to attach mouse to it. 
+Move window using arrow keys and press "ENTER" to confirm move.
+
+#>
+
+<# Reboot Windows cheat sheet 
+Source: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/shutdown
+
+Reboot into UEFI settings
+shutdown -t 0 -r -fw
+
+Reboot into Advanced startup
+shutdown -t 0 -r -o
+
+Reboot computer and automatically logon to current session
+shutdown -g -t 0
+
+Reboot another computer
+shutdown -m \\IP-or-DNS -t 0 -r
+
+#>
