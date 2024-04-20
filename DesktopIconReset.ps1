4b8ec772419b0f87af2c60e72154894056ab2d8b
Daniel Wahlfridsson <43478449+dawah90@users.noreply.github.com>
2024-04-20 18:42:33 +0200

    Add files via upload

---
+++ DesktopIconReset.ps1
@@ -0,0 +1,3 @@
+﻿Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes" -Name "ThemeChangesDesktopIcons" -Value 0 -Verbose
+Get-ItemProperty -Path "C:\$env:HOMEPATH\documents\desktop.ini" -ErrorAction SilentlyContinue | Remove-Item -Force -Verbose
+Get-ItemProperty -Path "$env:LOCALAPPDATA\iconcache.db" -ErrorAction SilentlyContinue | Remove-Item -Force -Verbose
